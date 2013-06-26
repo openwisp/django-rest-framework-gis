8171af80ab1bff2ffac4b85642217a37fb485d74
Christoph Heer <Christoph.Heer@gmail.com>
2013-06-26 23:12:03 +0200

    Rewrite serializer

--- rest_framework_gis/serializers.py
+++ rest_framework_gis/serializers.py
@@ -5,62 +5,18 @@ from rest_framework.serializers import ModelSerializer
 
 from .fields import GeometryField
 
+
 class GeoModelSerializer(ModelSerializer):
-    
-    def get_field(self, model_field):
-        """
-        Creates a default instance of a basic non-relational field.
-        """
-        kwargs = {}
 
-        kwargs['blank'] = model_field.blank
+    pass
 
-        if model_field.null or model_field.blank:
-            kwargs['required'] = False
-
-        if isinstance(model_field, models.AutoField) or not model_field.editable:
-            kwargs['read_only'] = True
-
-        if model_field.has_default():
-            kwargs['required'] = False
-            kwargs['default'] = model_field.get_default()
-
-        if issubclass(model_field.__class__, models.TextField):
-            kwargs['widget'] = widgets.Textarea
-
-        # TODO: TypedChoiceField?
-        if model_field.flatchoices:  # This ModelField contains choices
-            kwargs['choices'] = model_field.flatchoices
-            return ChoiceField(**kwargs)
-
-        field_mapping = {
-            models.AutoField: IntegerField,
-            models.FloatField: FloatField,
-            models.IntegerField: IntegerField,
-            models.PositiveIntegerField: IntegerField,
-            models.SmallIntegerField: IntegerField,
-            models.PositiveSmallIntegerField: IntegerField,
-            models.DateTimeField: DateTimeField,
-            models.EmailField: EmailField,
-            models.CharField: CharField,
-            models.URLField: URLField,
-            models.SlugField: SlugField,
-            models.TextField: CharField,
-            models.CommaSeparatedIntegerField: CharField,
-            models.BooleanField: BooleanField,
-            models.FileField: FileField,
-            models.ImageField: ImageField,
-            models.GeometryField: GeometryField,
-            models.PointField: GeometryField,
-            models.LineStringField: GeometryField,
-            models.PolygonField: GeometryField,
-            models.MultiPointField: GeometryField,
-            models.MultiLineStringField: GeometryField,
-            models.MultiPolygonField: GeometryField,
-            models.GeometryCollectionField: GeometryField
-        }
-
-        try:
-            return field_mapping[model_field.__class__](**kwargs)
-        except KeyError:
-            return ModelField(model_field=model_field, **kwargs)
+GeoModelSerializer.field_mapping.update({
+    models.GeometryField: GeometryField,
+    models.PointField: GeometryField,
+    models.LineStringField: GeometryField,
+    models.PolygonField: GeometryField,
+    models.MultiPointField: GeometryField,
+    models.MultiLineStringField: GeometryField,
+    models.MultiPolygonField: GeometryField,
+    models.GeometryCollectionField: GeometryField
+})
