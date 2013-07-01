6254693a8aa44722563c92443f8ddd834be9c641
Douglas Meehan <dmeehan@gmail.com>
2013-07-01 16:18:17 +0000

    added licesnse to setup

--- setup.py
+++ setup.py
@@ -6,12 +6,23 @@ setup(
     name='djangorestframework-gis',
     version=__version__,
     url='https://github.com/dmeehan/django-rest-framework-gis',
-    license='',
+    license='BSD',
     author='Douglas Meehan',
     author_email='dmeehan@gmail.com',
     description='Geographic add-ons for Django Rest Framework',
     packages=find_packages(),
     install_requires=[
         "djangorestframework>=2.2.2"
+    ]
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
     ]
 )
