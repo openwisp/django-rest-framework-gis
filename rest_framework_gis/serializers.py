660043043d8d3400fa7c83596c77edad77eccaba
Douglas Meehan <dmeehan@gmail.com>
2013-07-17 22:45:12 +0000

    allow for list or tuple of field names

--- rest_framework_gis/serializers.py
+++ rest_framework_gis/serializers.py
@@ -49,7 +49,7 @@ class GeoFeatureModelSerializer(GeoModelSerializer):
             # if 'fields' are declared, make sure it includes 'geo_field'
             if self.opts.fields:
                if self.opts.geo_field not in self.opts.fields:
-                   self.opts.fields = list(self.opts.fields)
+                   #self.opts.fields = list(self.opts.fields)
                    self.opts.fields.append(self.opts.geo_field)
 
 
