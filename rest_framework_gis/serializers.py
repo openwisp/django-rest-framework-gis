d66d627ca1d1aa9940aa72e118b7b48970f54703
Douglas Meehan <dmeehan@gmail.com>
2013-07-17 17:48:34 +0000

    fixed indent error

--- rest_framework_gis/serializers.py
+++ rest_framework_gis/serializers.py
@@ -53,23 +53,23 @@ class GeoFeatureModelSerializer(GeoModelSerializer):
 
     def to_native(self, obj):
         """
-        Serialize objects -> primitives.
+            Serialize objects -> primitives.
         """
         ret = self._dict_class()
-        ret["type"] = "Feature"
+        ret["type"] = "Feature" 
         ret["properties"] = {}
         ret["geometry"] = {}
-
+        
         for field_name, field in self.fields.items():
-        	field.initialize(parent=self, field_name=field_name)
+            field.initialize(parent=self, field_name=field_name)
             key = self.get_field_key(field_name)
             value = field.field_to_native(obj, field_name)
             if field_name == self.opts.geo_field:
                 ret["geometry"] = value
             else:
-            	ret["properties"][key] = value
-            	ret.fields[key] = field
-        return ret
+                ret["properties"][key] = value
+                ret.fields[key] = field
+            return ret
 
     def from_native:
     	pass
