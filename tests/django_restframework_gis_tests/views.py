f9f8d58488baaaeb7dc9978d9b9cd86b5c265b19
theflyingnerd <theflyingnerd@users.noreply.github.com>
2013-11-07 12:26:41 +1300

    Added test views for InBBoxFilter

--- tests/django_restframework_gis_tests/views.py
+++ tests/django_restframework_gis_tests/views.py
@@ -29,8 +29,24 @@ class GeojsonLocationList(generics.ListCreateAPIView):
     paginate_by = 40
     
 geojson_location_list = GeojsonLocationList.as_view()
-    
-    
+
+
+class GeojsonLocationContainedInBBoxList(generics.ListAPIView):
+    model = Location
+    serializer_class = LocationGeoFeatureSerializer
+    queryset = Location.objects.all()
+    bbox_filter_field = 'geometry'
+    filter_backends = (InBBoxFilter,)
+
+geojson_location_contained_in_bbox_list = GeojsonLocationContainedInBBoxList.as_view()
+
+
+class GeojsonLocationOverlapsBBoxList(GeojsonLocationContainedInBBoxList):
+    bbox_filter_include_overlapping = True
+
+geojson_location_overlaps_bbox_list = GeojsonLocationOverlapsBBoxList.as_view()
+
+
 class GeojsonLocationDetails(generics.RetrieveUpdateDestroyAPIView):
     model = Location
     serializer_class = LocationGeoFeatureSerializer
