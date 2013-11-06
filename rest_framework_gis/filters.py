987d6b1dacedf0dceea4f2abdc3f351c6f9a3014
theflyingnerd <theflyingnerd@users.noreply.github.com>
2013-11-06 16:50:27 +1300

    Added bbox_filter_include_overlapping flag to allow overlap-inclusive as well as containment filtering.

--- rest_framework_gis/filters.py
+++ rest_framework_gis/filters.py
@@ -25,6 +25,11 @@ class InBBOXFilter(BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         filter_field = getattr(view, 'bbox_filter_field', None)
+        include_overlapping = getattr(view, 'bbox_filter_include_overlapping', False)
+        if include_overlapping:
+            geoDjango_filter = 'bboverlaps'
+        else:
+            geoDjango_filter = 'bbcontains'
 
         if not filter_field:
             return queryset
@@ -32,4 +37,4 @@ class InBBOXFilter(BaseFilterBackend):
         bbox = self.get_filter_bbox(request)
         if not bbox:
             return queryset
-        return queryset.filter(Q(**{filter_field + '__contained': bbox}))
+        return queryset.filter(Q(**{'%s__%s' % (filter_field, geoDjango_filter): bbox}))
