ff8ad003c18775bdc66746e002097f270b54785f
auvipy <auvipy@gmail.com>
2015-12-05 19:10:04 +0600

    cleaup tests url

--- tests/urls.py
+++ tests/urls.py
@@ -1,16 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',    
+urlpatterns = [
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
+
     url(r'', include('django_restframework_gis_tests.urls')),
-    
+
     url(r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views.serve'),
-)
+
+]
