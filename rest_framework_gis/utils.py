2aadadda2d59d5ad2e6872a2e690e401ab50e18d
auvipy <auvipy@gmail.com>
2015-12-07 10:33:07 +0600

    removed py2.6 compat

--- rest_framework_gis/utils.py
+++ rest_framework_gis/utils.py
@@ -1,5 +1,1 @@
-try:
-    from collections import OrderedDict  # noqa
-# python 2.6
-except ImportError:  # pragma: no cover
-    from ordereddict import OrderedDict  # noqa
+from collections import OrderedDict  # noqa
