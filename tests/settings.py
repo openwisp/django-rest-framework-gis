e1f35515ef20084f55da60fc9c6ae10896593404
Federico Capoano <nemesis@ninux.org>
2016-04-04 10:18:43 +0200

    [tests] Removed security middleware from test settings

--- tests/settings.py
+++ tests/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
