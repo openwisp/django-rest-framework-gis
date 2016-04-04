93f3627d5cf85f2cb6eb0a3c272b438136874377
Federico Capoano <nemesis@ninux.org>
2016-04-04 10:15:54 +0200

    [tests] Fixed middleware settings

--- tests/settings.py
+++ tests/settings.py
@@ -35,8 +35,13 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'urls'
