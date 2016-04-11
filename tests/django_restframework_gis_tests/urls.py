fad43f54174033d64c8862fde2638439bb836ee1
auvipy <auvipy@gmail.com>
2016-04-12 00:25:24 +0600

    used relative import for views

--- tests/django_restframework_gis_tests/urls.py
+++ tests/django_restframework_gis_tests/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .django_restframework_gis_tests import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.location_list, name='api_location_list'),
