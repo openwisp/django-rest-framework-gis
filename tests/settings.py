625a36b176d12878c654333a81a5004836ee33db
Federico Capoano <nemesis@ninux.org>
2016-04-13 10:23:04 +0200

    Added TEMPLATES setting to test project #104
    Tested on django 1.10 prealpha successfully.
    Closes #104

--- tests/settings.py
+++ tests/settings.py
@@ -43,6 +43,22 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = 'urls'
 
 TIME_ZONE = 'Europe/Rome'
