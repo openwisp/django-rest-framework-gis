5b5bc444789cc1de41fc1c08b132db9bcf461601
Austin Riba <ariba@lcogt.net>
2016-09-26 03:22:40 +0000

    maintain bakwards compatibility for old versions of django-filter

--- rest_framework_gis/filters.py
+++ rest_framework_gis/filters.py
@@ -84,7 +84,10 @@ class GeoFilterSet(django_filters.FilterSet):
     }
 
     def __new__(cls, *args, **kwargs):
-        cls._meta.filter_overrides.update(cls.GEOFILTER_FOR_DBFIELD_DEFAULTS)
+        try:
+            cls._meta.filter_overrides.update(cls.GEOFILTER_FOR_DBFIELD_DEFAULTS)
+        except AttributeError:  # Compatibility for django-filter < 0.15
+            cls.filter_overrides.update(cls.GEOFILTER_FOR_DBFIELD_DEFAULTS)
         cls.LOOKUP_TYPES = sorted(gis_lookups)
         return super(GeoFilterSet, cls).__new__(cls)
 
