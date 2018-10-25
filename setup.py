424fb444d7b28c7e90bd6731c3a3da6b930ef215
Ryan P Kilby <kilbyr@gmail.com>
2018-10-25 16:13:52 -0700

    Fix trove classifiers

--- setup.py
+++ setup.py
@@ -52,10 +52,11 @@ setup(
         'Operating System :: OS Independent',
         'Framework :: Django',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
     ]
 )
