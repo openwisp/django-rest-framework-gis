08598776cef07516697ce402ba72e9ef4e0bc117
Dhaval Mehta <20968146+dhaval-mehta@users.noreply.github.com>
2020-03-19 02:48:13 +0530

    add code to generate schema from serializer

---
+++ rest_framework_gis/schema.py
@@ -0,0 +1,164 @@
+import warnings
+
+from django.contrib.gis.db import models
+from rest_framework.schemas.openapi import AutoSchema
+from rest_framework.utils import model_meta
+
+from rest_framework_gis.serializers import GeoFeatureModelListSerializer, GeoFeatureModelSerializer
+
+
+class GeoFeatureAutoSchema(AutoSchema):
+    POINT_SCHEMA = {
+        'type': 'array',
+        'items': {
+            'type': 'number',
+            'format': 'float',
+        },
+        'example': [12.9721, 77.5933],
+        'minItems': 2,
+        'maxItems': 3,
+    }
+
+    LINE_STRING_SCHEMA = {
+        'type': 'array',
+        'items': POINT_SCHEMA,
+        'example': [[22.4707, 70.0577], [12.9721, 77.5933]],
+        'minItems': 2,
+    }
+
+    GEO_FIELD_TO_SCHEMA = {
+        models.PointField: {
+            'type': {
+                'type': 'string',
+                'enum': ['Point'],
+            },
+            'coordinates': POINT_SCHEMA,
+        },
+        models.LineStringField: {
+            'type': {
+                'type': 'string',
+                'enum': ['LineString'],
+            },
+            'coordinates': LINE_STRING_SCHEMA,
+        },
+        models.PolygonField: {
+            'type': {
+                'type': 'string',
+                'enum': ['LineString'],
+            },
+            'coordinates': {
+                'type': 'array',
+                'items': {
+                    **LINE_STRING_SCHEMA,
+                    'minItems': 4,
+                },
+                'minItems': 1,
+                'example': [[[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]],
+                            [[0.4, 0.4], [0.4, 0.6], [0.6, 0.6], [0.6, 0.4], [0.4, 0.4]]]
+            }
+        }
+    }
+
+    MULTI_FIELD_MAPPING = {
+        models.PointField: models.MultiPointField,
+        models.LineStringField: models.MultiLineStringField,
+        models.PolygonField: models.MultiPolygonField
+    }
+
+    for singular_field, multi_field in MULTI_FIELD_MAPPING.items():
+        GEO_FIELD_TO_SCHEMA[multi_field] = {
+            'type': {
+                'type': 'string',
+                'enum': [multi_field.geom_class.__name__],
+            },
+            'coordinates': {
+                'type': 'array',
+                'items': GEO_FIELD_TO_SCHEMA[singular_field]['coordinates'],
+                'minItems': 1,
+                'example': [
+                    GEO_FIELD_TO_SCHEMA[singular_field]['coordinates']['example'],
+                    GEO_FIELD_TO_SCHEMA[singular_field]['coordinates']['example']
+                ]
+            }
+        }
+
+    def _map_geo_field(self, serializer, geo_field_name):
+        field = serializer.fields[geo_field_name]
+        if field.source:
+            model_field_name = field.source
+        else:
+            model_field_name = geo_field_name
+
+        geo_field = model_meta.get_field_info(serializer.Meta.model).fields[model_field_name]
+        try:
+            return self.GEO_FIELD_TO_SCHEMA[geo_field.__class__]
+        except KeyError:
+            warnings.warn('Geometry generation for {field} is not supported.'.format(field=field))
+            return {}
+
+    def _map_field(self, field):
+        if isinstance(field, GeoFeatureModelListSerializer):
+            return self._map_geo_feature_model_list_serializer(field)
+
+        return super()._map_field(field)
+
+    def _map_geo_feature_model_list_serializer(self, serializer):
+        return {
+            'type': 'object',
+            'properties': {
+                'type': {
+                    'type': 'string',
+                    'enum': ['FeatureCollection']
+                },
+                'features': {
+                    'type': 'array',
+                    'items': self._map_serializer(serializer.child)
+                }
+            }
+        }
+
+    def _map_geo_feature_model_serializer(self, serializer):
+        schema = super()._map_serializer(serializer)
+
+        geo_json_schema = {
+            'type': 'object',
+            'properties': {
+                'type': {
+                    'type': 'string',
+                    'enum': ['Feature']
+                },
+            }
+        }
+
+        if serializer.Meta.id_field:
+            geo_json_schema['id'] = schema['properties'].pop(serializer.Meta.id_field)
+
+        geo_field = serializer.Meta.geo_field
+        geo_json_schema['properties']['geometry'] = {
+            'type': 'object',
+            'properties': self._map_geo_field(serializer, geo_field),
+        }
+        schema['properties'].pop(geo_field)
+
+        if serializer.Meta.auto_bbox or serializer.Meta.bbox_geo_field:
+            geo_json_schema['properties']['bbox'] = {
+                'type': 'array',
+                'minItems': 4,
+                'maxItems': 4,
+                'example': [12.9721, 77.5933, 12.9721, 77.5933],
+            }
+            if serializer.Meta.bbox_geo_field in schema:
+                schema.pop(serializer.Meta.bbox_geo_field)
+
+        geo_json_schema['properties']['properties'] = schema
+
+        return geo_json_schema
+
+    def _map_serializer(self, serializer):
+        if isinstance(serializer, GeoFeatureModelListSerializer):
+            return self._map_geo_feature_model_list_serializer(serializer)
+
+        if isinstance(serializer, GeoFeatureModelSerializer):
+            return self._map_geo_feature_model_serializer(serializer)
+
+        return super()._map_serializer(serializer)
