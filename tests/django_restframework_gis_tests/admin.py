3fb07a5b6f70ac965c353215efdee93d6f34cc2f
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-10-31 12:18:33 +0300

    Modernize the code for Django 4.2
    with the help of django-upgrade
    
    https://github.com/adamchainz/django-upgrade

--- tests/django_restframework_gis_tests/admin.py
+++ tests/django_restframework_gis_tests/admin.py
@@ -4,8 +4,8 @@ from django.contrib.gis.admin import ModelAdmin as GeoModelAdmin
 from .models import Location
 
 
+@admin.register(Location)
 class LocationAdmin(GeoModelAdmin):
     list_display = ('name', 'geometry')
 
 
-admin.site.register(Location, LocationAdmin)
