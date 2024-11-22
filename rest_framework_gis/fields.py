7cf2ba0229a3778f3afc0d96e3890130f5d1f68a
Federico Capoano <f.capoano@openwisp.io>
2024-11-22 17:59:58 -0300

    [chores] Updated GeoJsonDict docstring

--- rest_framework_gis/fields.py
+++ rest_framework_gis/fields.py
@@ -125,8 +125,7 @@ class GeometrySerializerMethodField(SerializerMethodField):
 
 class GeoJsonDict(OrderedDict):
     """
-    Used for serializing GIS values to GeoJSON values
-    TODO: remove this when support for python 2.6/2.7 will be dropped
+    Used for serializing GIS values to GeoJSON values.
     """
 
     def __init__(self, *args, **kwargs):
