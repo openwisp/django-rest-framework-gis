04949b59848da720d6aafbf7388415e1e4ac2eaa
Federico Capoano <f.capoano@openwisp.io>
2026-08-04 15:15:31 -0300

    [change] Dropped Python 3.9 support

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "Framework :: Django",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
