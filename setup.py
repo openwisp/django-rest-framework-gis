d41806f33d583650d68275fad53cf658541976db
Francisco Puga <fpuga@icarto.es>
2026-08-26 14:18:02 +0200

    [deps] Bumped django-filter to >=23.5,<27.0 #373
    Bumped django-filter dependency upper bound to permit version 26.x.
    
    Fixes #373

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "django>=4.2",
         "djangorestframework>=3.12,<3.19",
-        "django-filter>=23.5,<26.0",
+        "django-filter>=23.5,<27.0",
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
